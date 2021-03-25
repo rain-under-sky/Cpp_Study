ee13cb35e98fc51eecfc68b405091da60672a007
aomsir <info@say521.cn>
2021-03-25 21:00:45 +0800

    1

---
+++ cmake-build-debug/CMakeFiles/Cpp_Study.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cpp_Study.dir/main.cpp.o"
+  "Cpp_Study"
+  "Cpp_Study.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cpp_Study.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
-using namespace std;
 
-int main(){
-    int *p = new int(5);
-    int *p1 = new int(6);
-    cout<<*p<<endl;
-    cout<<*p1<<endl;
-
+int main() {
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
