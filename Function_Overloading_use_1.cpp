e34cd22f488e3cd6281e32e7a2046f45fbe5d98f
aomsir <info@say521.cn>
2021-03-28 15:13:57 +0800

    commit

--- Function_Overloading_use_1.cpp
+++ Function_Overloading_use_1.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 int Add(int a,int b);
-float Add(float a,float b);
+double Add(double a,double b);
 
 int main(void){
     cout<<Add(1,2)<<endl;
-    cout<<Add(1.200000,1.300000)<<endl;
+    cout<<Add(1.2,1.3)<<endl;
 
     return 0;
 }
@@ -14,6 +14,6 @@ int Add(int a,int b){
     return a+b;
 }
 
-float Add(float a,float b){
+double Add(double a,double b){
     return a+b;
 }
