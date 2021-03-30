8c9856bd0eadf9056973904b5a68906f36c3a46f
aomsir <info@say521.cn>
2021-03-30 22:02:32 +0800

    commit

--- class_use_3.cpp
+++ class_use_3.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 void test();
 
+//定义一个teacher类
 class teacher{
 private:
     string name;
@@ -15,15 +16,15 @@ private:
         cout<<name<<' '<<old<<endl;
     }
 
-    friend class student;   //给student类加友元
-    friend void test();
+    friend class student;   //给student类加友元，即加白名单
+    friend void test();     //给test函数加又元，即加白名单
 };
 
 class student{
 public:
-    teacher tea1;
+    teacher tea1;   //在student类里面声明一个teacher类变量
     void fun2(){
-        tea1.fun1();
+        tea1.fun1();   //在student类里面通过tea1变量访问fun1函数
     }
 };
 
