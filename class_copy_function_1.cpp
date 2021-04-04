f5a4e2be669135d40fac0ea086cab4f4fc1439b1
aomsir <info@say521.cn>
2021-04-04 15:17:58 +0800

    commit

---
+++ class_copy_function_1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class student{
+private:
+    string name;
+    int age;
+
+public:
+    student(string *pname,int page){
+        name = *pname;
+        age = page;
+    }
+
+    void print(){
+        cout<<name<<' '<<endl;
+        cout<<name<<' '<<endl;
+    }
+
+    ~student(){};
+};
+
+int main(){
+    student stu1("aomsir",18);
+    student stu2(stu1);
+    stu1.print();
+    stu2.print();
+
+    return 0;
+}
