8264abcff9e01033267cd3fdacad59f1ad0ac600
aomsir <info@say521.cn>
2021-04-24 21:06:33 +0800

    1

--- 派生与继承/paisheng.cpp
+++ 派生与继承/paisheng.cpp
@@ -3,5 +3,19 @@ using namespace std;
 
 class person{
 private:
-    
+    string name;
+    int old;
+public:
+    person(){
+
+    }
+
+    ~person(){
+        cout<<"析构结束"<<endl;
+    }
+
+    void setperson(string pname,int pold){
+        old = pold;
+        name = pname;
+    }
 };
