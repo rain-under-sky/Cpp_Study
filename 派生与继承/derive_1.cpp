56958a2a1e31c2d1c2dd744e3ffef1fb8bb9bdff
aomsir <info@say521.cn>
2021-04-25 19:24:08 +0800

    1

--- 派生与继承/derive_1.cpp
+++ 派生与继承/derive_1.cpp
@@ -19,18 +19,32 @@ public:
         old = pold;
         name = pname;
     }
+
+    void printf(){
+        cout<<name<<endl;
+        cout<<old<<endl;
+    }
 };
 
 class student:public person{
 private:
-    int ID;
+    int ID;  //子类特有属性
 public:
-    void SetStudent(string pname,int pold,int pID){
-        person::SetPerson(string pname,int pold);
+    void SetStudent(string pname,int pold,int pID){  //参数是把所有的内容传进来，然后依次给下面的基类成员函数复制，然后赋值给基类的私有
+        SetPerson(pname,pold); //把对应参数的值传进基类的private成员，因为子类无法访问
         ID = pID;
+    }
+
+    void print(){  //调用这个函数的时候会依次调用基类的函数的对应函数
+        printf();  //调用基类的成员函数，假如基类的成员函数名字和这个一样，功能也一样就需要加基类名::，如果不同名就直接写基类的成员函数名
+        cout<<ID<<endl;
     }
 };
 
 int main(){
+    student hxh;
+    hxh.SetStudent("hxh",19,10101);
+    hxh.print();
 
+    return 0;
 }
