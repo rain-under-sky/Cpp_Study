111c321be02df8d25f2c30cac8b060efd430268b
aomsir <info@say521.cn>
2021-05-16 08:22:38 +0800

    1

--- StudentScoreMangeSysytem.c
+++ StudentScoreMangeSysytem.c
@@ -6,6 +6,21 @@ struct student{
 
 };
 
+int main(void){
+
+}
+
+void InforOut(){
+    printf("********************************");
+    printf("欢迎来到学生成绩管理系统");
+    printf("");
+    printf();
+    printf();
+    printf();
+    printf("********************************");
+
+}
+
 
 
 
