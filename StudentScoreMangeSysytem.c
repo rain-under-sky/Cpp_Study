677851b7f84634f57a91ada54a0aacb4fe572976
aomsir <info@say521.cn>
2021-05-16 09:28:47 +0800

    1

--- StudentScoreMangeSysytem.c
+++ StudentScoreMangeSysytem.c
@@ -10,8 +10,9 @@ struct student{    //学生基本信息的结构体
     int MathScore;
     int EnglishScore;
     int ScienceScore;
+    struct student *next;
 };
-void InforOut(){   //控制台输出的函数
+/*void InforOut(){   //控制台输出的函数
     printf("********************************");
     printf("欢迎来到学生成绩管理系统");
     printf("");
@@ -20,19 +21,33 @@ void InforOut(){   //控制台输出的函数
     printf(1);
     printf("********************************");
 
-}
+}*/
 
 
 
 
 int main(void){
-    int StudentCount;
-    printf("请输入您的学生个数:");
-    scanf("%d",&StudentCount);
     struct student *head;
     struct student *tail,*start;
-    head = tail = (int*)malloc(sizeof(head));
-    
+    head = tail = (int *)malloc(70);
+    int StudentCount; //学生人数
+    int i = 1;  //链表控制变量
+    printf("请输入您的学生个数:");
+    scanf("%d",&StudentCount);
+    printf("请依次输入第%d个学生的姓名，学号，ID，数学成绩，英语成绩，计算机成绩",&i);
+    scanf("%c%d%d%d%d",tail->name[15],tail->ID,tail->MathScore,tail->EnglishScore,tail->ScienceScore);
+
+    for(i = 2;i < StudentCount;i++){
+        start = (int*)malloc(sizeof(start));
+        printf("请输入第i个学生的姓名，学号，ID，数学成绩，英语成绩，计算机成绩",&i);
+        scanf("%s%d%d%d%d",tail->name[15],tail->ID,tail->MathScore,tail->EnglishScore,tail->ScienceScore);
+        tail->next = start;
+        tail = start;
+    }
+    tail->next = NULL;
+    start = head; //将start指针指到head指针上，等下用start指针进行循环遍历
+
+
 
 }
 
