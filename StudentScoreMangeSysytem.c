fbc93e83568ec56757e008c73daf8b5558b3d33b
aomsir <info@say521.cn>
2021-05-16 08:33:46 +0800

    Update StudentScoreMangeSysytem.c

--- StudentScoreMangeSysytem.c
+++ StudentScoreMangeSysytem.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-struct student{    //学生基本信息
+struct student{    //学生基本信息的结构体
     char name[15];
     int ID;
     int class;
@@ -9,7 +9,7 @@ struct student{    //学生基本信息
     int EnglishScore;
     int ScienceScore;
 };
-void InforOut(){
+void InforOut(){   //控制台输出的函数
     printf("********************************");
     printf("欢迎来到学生成绩管理系统");
     printf("");
