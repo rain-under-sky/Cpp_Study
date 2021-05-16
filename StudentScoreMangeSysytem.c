e70434dde62f12f02f291e4c180380e68bc242a1
aomsir <info@say521.cn>
2021-05-16 11:24:51 +0800

    1

--- StudentScoreMangeSysytem.c
+++ StudentScoreMangeSysytem.c
@@ -29,24 +29,62 @@ struct student{    //学生基本信息的结构体
 int main(void){
     int StudentCount; //学生人数
     int i = 1;  //链表控制变量
+    char CheckName[15];
+    int CheckID;
     printf("请输入您的学生个数:");
     scanf("%d",&StudentCount);
 
+    /*以下代码是成绩的输入*/
     struct student *head;
     struct student *tail,*start;
     head = tail = (struct student*)malloc(sizeof(struct student));
     printf("请依次输入第%d个学生的姓名，学号，ID，数学成绩，英语成绩，计算机成绩:",i);
-    scanf("%s%d%d%d%d",tail->name[15],&tail->ID,&tail->MathScore,&tail->EnglishScore,&tail->ComputerScore);
+    scanf("%s%d%d%d%d",&tail->name[15],&tail->ID,&tail->class,&tail->MathScore,&tail->EnglishScore,&tail->ComputerScore);
 
     for(i = 2;i < StudentCount;i++){
         start = (struct student*)malloc(sizeof(struct student));
         printf("请依次输入第%d个学生的姓名，学号，ID，数学成绩，英语成绩，计算机成绩:",i);
-        scanf("%s%d%d%d%d",tail->name[15],&tail->ID,&tail->MathScore,&tail->EnglishScore,&tail->ComputerScore);
+        scanf("%s%d%d%d%d",&tail->name[15],&tail->ID,&tail->class,&tail->MathScore,&tail->EnglishScore,&tail->ComputerScore);
         tail->next = start;
         tail = start;
     }
     tail->next = NULL;
     start = head; //将start指针指到head指针上，等下用start指针进行循环遍历
+
+    /*以下代码是学生学号的查询*/
+
+    scanf("%d",CheckID);
+    while(start != NULL){
+        if(CheckID == start->ID){
+            printf("*************************************");
+            printf("该学生信息如下");
+            printf("姓名:%s\n",start->name[15]);
+            printf("学号:%d\n",start->ID);
+            printf("班级:%d\n",start->class);
+            printf("数学成绩\n:%d",start->MathScore);
+            printf("英语成绩:%d\n",start->EnglishScore);
+            printf("英语成绩:%d\n",start->ComputerScore);
+            printf("*************************************");
+        }
+        start = start->next;
+    }
+
+    printf("请输入您的学生姓名进行查询");
+    scanf("%s",CheckName[15]);
+    while(start != NULL){
+        if(CheckID == start->name[15]){
+            printf("*************************************");
+            printf("该学生信息如下");
+            printf("姓名:%s\n",start->name[15]);
+            printf("学号:%d\n",start->ID);
+            printf("班级:%d\n",start->class);
+            printf("数学成绩\n:%d",start->MathScore);
+            printf("英语成绩:%d\n",start->EnglishScore);
+            printf("英语成绩:%d\n",start->ComputerScore);
+            printf("*************************************");
+        }
+        start = start->next;
+    }
 
 }
 
