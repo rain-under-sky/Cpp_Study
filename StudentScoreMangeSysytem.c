738c412ee5dce9b730e2c8144955f659d3a23518
aomsir <info@say521.cn>
2021-05-16 08:33:14 +0800

    1

--- StudentScoreMangeSysytem.c
+++ StudentScoreMangeSysytem.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+struct student{    //学生基本信息
+    char name[15];
+    int ID;
+    int class;
+    int MathScore;
+    int EnglishScore;
+    int ScienceScore;
+};
 void InforOut(){
     printf("********************************");
     printf("欢迎来到学生成绩管理系统");
@@ -11,9 +19,8 @@ void InforOut(){
     printf("********************************");
 
 }
-struct student{
 
-};
+
 
 
 int main(void){
