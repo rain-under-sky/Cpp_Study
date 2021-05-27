a1a6e034fb36a81459a8d693a494cd9187eca713
aomsir <info@say521.cn>
2021-05-27 12:09:10 +0800

    1

--- cmake-build-debug/CMakeFiles/test.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/test.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/test.dir/C语言测试/test.c.o"
+  "CMakeFiles/test.dir/test.cpp.o"
   "test"
   "test.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang C)
+foreach(lang CXX)
   include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
--- test.cpp
+++ test.cpp
@@ -1,114 +1,30 @@
-worker* way(worker *head)						//排序函数
+#include "stdio.h"
+#include <string.h>
+
+// pbin：待转换的二进制字符串。
+// 返回值：二进制字符串转换为十进制整数的结果。
+long bintodec(const char *pbin)  //参数传递为常指针，以免字符指针修改了外部的值
 {
-    worker *p=NULL,*p1=NULL,*p2=NULL,*p3=NULL;
-    int num=0,i,j,k,min,num_1;
-    worker* temp=NULL;
-    p =p1= head;
-    if (head != NULL)							//判断是文件是否存在
-    {
-        while (1)
-        {
-            cout << "请选择排序方式：1：总工资排序 2：基本工资排序" << endl;
-            cin >> num_1;
-            if(num_1==1||num_1==2)											//选择排序方式
-            {
-                if(num_1==1)												//总工资排序
-                {
-                    while (p != NULL)										//计算总人数
-                    {
-                        p = p->next;
-                        num++;
-                    }
-                    p2 = new worker[num];									//新建数组
-                    if (!p2)
-                    {
-                        cout << "创建内存失败" << endl;
-                    }
-                    for(int i=0;i<num;i++)									//把链表的数据拷贝到数组中
-                    {
-                        *(p2 + i) = *p1;
-                        p1=p1->next;
-                    }
-                    temp = new worker[1];
-                    for (int i = 0; i < num - 1; i++)							//给数据升序
-                    {
-                        for (int j = 0; j < num - i - 1; j++)
-                        {
-                            if ((p2+j)->all_wage > (p2+j+1)->all_wage)
-                            {
-                                copy(temp,p2+j);
-                                copy(p2+j, p2+j+1);
-                                copy(p2+j+1, temp);
-                            }
-                        }
-                    }
-                    p1 = head;
-                    for(int i=num-1;i>=0;i--)								//把数组中的数据按降序拷贝到链表
-                    {
-                        copy(p1, p2+i);
-                        //*p1=*(p2 + i);
-                        p1 = p1->next;
-                    }
-                    p1=head;
-                    delete[]p2;												//释放创建数组空间
-                    cout << "总工资排序成功" << endl;
-                    break;
-                }
-                else														//基本工资排序
-                {
-                    while (p != NULL)										//计算总人数
-                    {
-                        p = p->next;
-                        num++;
-                    }
-                    p2 = new worker[num];									//新建数组
-                    if (!p2)
-                    {
-                        cout << "创建内存失败" << endl;
-                    }
-                    for (int i = 0; i < num; i++)							//把链表的数据拷贝到数组中
-                    {
-                        *(p2 + i) = *p1;
-                        p1 = p1->next;
-                    }
-                    temp = new worker[1];
-                    for (int i = 0; i < num - 1; i++)							//给数据升序
-                    {
-                        for (int j = 0; j < num - i - 1; j++)
-                        {
-                            if ((p2 + j)->basis_wage> (p2 + j + 1)->basis_wage)
-                            {
-                                copy(temp, p2 + j);
-                                copy(p2 + j, p2 + j + 1);
-                                copy(p2 + j + 1, temp);
-                            }
-                        }
-                    }
-                    p1 = head;
-                    for (int i = num - 1; i >= 0; i--)									//把数组中的数据按降序拷贝到链表
-                    {
-                        copy(p1, p2 + i);
-                        //*p1=*(p2 + i);
-                        p1 = p1->next;
-                    }
+    int ii=0;
+    long result = 0;  //返回给函数外部的值
 
-                    p1 = head;
-                    cout << "基本工资排序成功" << endl;
-                    break;
-                }
-
-            }
-            else
-            {
-                cout << "输入有误，请重新输入" << endl;
-            }
-        }
-    }
-    else
+    while (pbin[ii]!=0)
     {
-        cout << "文件不存在" << endl;
+        result=result*2+(pbin[ii]-'0');
+        ii++;
     }
-    system("pause");
-    system("cls");
-    return p1;
+
+    return result;
 }
+
+int main()
+{
+    char str[65];
+
+    memset(str,0,sizeof(str));
+    printf("请输入一个二进制的字符串（只能由0和1组成）：");
+    scanf("%s",str);
+
+    printf("%s转换为十进制的结果是：%d\n",str,bintodec(str));
+}
+
