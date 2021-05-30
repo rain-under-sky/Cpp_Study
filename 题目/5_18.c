963744886c43ffeae5cfb2cf5f9cc2cda28444c0
aomsir <info@say521.cn>
2021-05-30 11:49:57 +0800

    1

--- 题目/5_18.c
+++ 题目/5_18.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 
 int main(void){
-    int a[20] = {1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39};
-    int CheckNum;
+    int a[20] = {1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39};  //数组里面元素的值必须是有序的，如果是无序的，先排序再整
+    int CheckNum;   //需要查尊的数
     int low = 0;
     int high = 19;
-    int mid = 9;
+    int mid = 9;   //中间数
     int flag = 0;
     printf("请输入您要查找的数：");
     scanf("%d",&CheckNum);
     while(low < high){
         mid = (low + high)/2;
-        //printf("low=%d,mid=%d,high=%d\n",low,mid,high);
         if(a[mid]>CheckNum)
         {
             high = high - 1;   //假如需要查找的数比现在最中间的数小，那么就把中间的数往前调1，再进行循环寻找
         }
         else if(a[mid]<CheckNum)
         {
-            low = low + 1;
+            low = low + 1;     //与上面的原理类似
         }
         else if(a[mid]==CheckNum)
         {
