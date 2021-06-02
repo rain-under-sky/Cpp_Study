ce1a6f7f1f47b4b5519a2abd255a81acb7cc0c84
aomsir <info@say521.cn>
2021-06-02 19:51:50 +0800

    1

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -64,5 +64,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/test1.dir/DependInfo.cmake"
   "CMakeFiles/Code.dir/DependInfo.cmake"
   )
--- test1.cpp
+++ test1.cpp
@@ -2,5 +2,13 @@
 #include <string.h>
 
 int main(void){
-    
+    char num[4];
+    int sum;
+    int i;
+    scanf("%s",num);
+    for(i=0;i<3;i++){
+        sum+=num[i];
+    }
+
+    printf("%d",sum);
 }
